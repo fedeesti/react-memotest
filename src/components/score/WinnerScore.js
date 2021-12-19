073af107a94cdc130188a4ca90eaba77bdc50a3a
fedeesti <fede.estigarribia@hotmail.com>
2021-12-19 19:48:17 -0300

    fix css

--- src/components/score/WinnerScore.js
+++ src/components/score/WinnerScore.js
@@ -2,7 +2,7 @@ import './WinnerScore.css';
 
 const WinnerScore = ({ show, result, restart = () => {} }) => {
     return (
-      <div className={'winner-card ' + (show ? '' : 'winner-score--hidden')}>
+      <div className={'winner-card' + (show ? '--hidden' : '')}>
         <span className="winner-card-text">
           Has completado el juego en {result}
         </span>
